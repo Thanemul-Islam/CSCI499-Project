4e126417f6d8fb23cffefc0bd40394c136f23382
D5H4R13 <32347012+D5H4R13@users.noreply.github.com>
2024-03-31 19:47:15 -0400

    changed number
    level select -> levelselect 1

--- scene/world_icon.gd
+++ scene/world_icon.gd
@@ -2,7 +2,7 @@
 extends Control
 
 @export var world_index: int = 1
-@export var level_select_packed: PackedScene = load("res://scene/level_select.tscn")
+@export var level_select_packed: PackedScene = load("res://scene/level_select1.tscn")
 @onready var level_select_scene: LevelSelect = level_select_packed.instantiate()
 
 # makes name of world

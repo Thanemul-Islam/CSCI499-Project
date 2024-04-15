f51c9e99a4249267321554c7ebad747ce79cbebe
kachowth <flameman741@hotmail.com>
2024-04-15 13:50:14 -0400

    forest levels
    first forest level, tutorial improvements, fixed falling spike and spinning blade
    
    i think normal spike is broken now though

--- scripts/spinningblade.gd
+++ scripts/spinningblade.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var path_follow: PathFollow2D = $Path2D/PathFollow2D
-@export var speed: int = 100
+@export var speed: int = 250
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,3 +11,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	path_follow.progress += speed * delta
+
+func _on_area_2d_area_entered(area):
+	if area.get_parent() is Player:
+		area.get_parent()._damage(1)

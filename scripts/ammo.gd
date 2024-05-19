aae11b5b732fcede481dc7468592ee81de559cbf
kyihune <102312781+kyihune@users.noreply.github.com>
2024-05-18 21:25:23 -0400

    Wiping everything
    Another hope at fixing these bugged things

--- scripts/ammo.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-
-func _on_area_2d_area_entered(area):
-	if area.get_parent() is Player:
-		GameManager.gain_ammo(5)
-		queue_free()
--- scripts/tonyinventory.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var coins = 0
-
-func add_coin():
-	coins += 1
